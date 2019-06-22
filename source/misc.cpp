9b6ed95ee86e6880fa38f7f53cd39f9078fa298d
Tama4649 <tama4649@gmail.com>
2019-06-23 00:30:17 +0900

    id name, authorをKristallweizenに変更。

--- source/misc.cpp
+++ source/misc.cpp
@@ -150,15 +150,16 @@ const string engine_info() {
 			ENGINE_NAME
 #endif			
 			<< ' '
-			<< EVAL_TYPE_NAME << ' '
-			<< ENGINE_VERSION << setfill('0')
-			<< (Is64Bit ? " 64" : " 32")
-			<< TARGET_CPU
+			<< "Kristallweizen"
+//			<< EVAL_TYPE_NAME << ' '
+//			<< ENGINE_VERSION << setfill('0')
+//			<< (Is64Bit ? " 64" : " 32")
+//			<< TARGET_CPU
 #if defined(FOR_TOURNAMENT)
 			<< " TOURNAMENT"
 #endif
 			<< endl
-			<< "id author by yaneurao" << endl;
+			<< "id author by たま" << endl;
 	}
 
 	return ss.str();

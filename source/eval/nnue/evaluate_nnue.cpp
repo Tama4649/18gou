8209b3969b81cc458943d424b786c6ea8d7e3c54
Tama4649 <tama4649@gmail.com>
2021-12-07 20:43:47 +0900

    evaluate_nnue.cppにCONFLICTの形跡が残ったままだったのを修正

--- source/eval/nnue/evaluate_nnue.cpp
+++ source/eval/nnue/evaluate_nnue.cpp
@@ -239,10 +239,6 @@ namespace Eval {
                     const std::string file_name = Path::Combine(dir_name, NNUE::kFileName);
                     std::ifstream stream(file_name, std::ios::binary);
                     sync_cout << "info string loading eval file : " << file_name << sync_endl;
-
-<<<<<<< HEAD
-		//	sync_cout << "info string loading eval file : " << file_name << sync_endl;
-=======
                     return NNUE::ReadParameters(stream);
                 }
                 else {
@@ -263,7 +259,6 @@ namespace Eval {
                     return NNUE::ReadParameters(stream);
                 }
             }();
->>>>>>> refs/remotes/origin/master
 
             //      ASSERT(result);
 

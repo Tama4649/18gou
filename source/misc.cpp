fdda7c40274c94b4714bc3af5c74b9ea90c67042
Tama4649 <tama4649@gmail.com>
2021-12-25 09:14:05 +0900

    やねうら王V7.00 2021/12/14 キャッチアップ

--- source/misc.cpp
+++ source/misc.cpp
@@ -1142,7 +1142,7 @@ namespace SystemIO
 	{
 #if defined(_MSC_VER)
 		return _ftelli64(f);
-#elif defined(__GNUC__) && defined(IS_64BIT) && !(defined(__ANDROID__) && defined(__ANDROID_API__) && __ANDROID_API__ < 24)
+#elif defined(__GNUC__) && defined(IS_64BIT) && !(defined(__ANDROID__) && defined(__ANDROID_API__) && __ANDROID_API__ < 24) && !defined(__MACH__)
 		return ftello64(f);
 #else
 		return ftell(f);
@@ -1153,7 +1153,7 @@ namespace SystemIO
 	{
 #if defined(_MSC_VER)
 		return _fseeki64(f, offset, origin);
-#elif defined(__GNUC__) && defined(IS_64BIT) && !(defined(__ANDROID__) && defined(__ANDROID_API__) && __ANDROID_API__ < 24)
+#elif defined(__GNUC__) && defined(IS_64BIT) && !(defined(__ANDROID__) && defined(__ANDROID_API__) && __ANDROID_API__ < 24) && !defined(__MACH__)
 		return fseeko64(f, offset, origin);
 #else
 		return fseek(f, offset, origin);
